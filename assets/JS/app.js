774dd90c904a4399aa9d6dded6500a397b43448d
unknown <jessrtrent@gmail.com>
2018-03-31 11:22:42 -0400

    new html

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,81 +1,52 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyAWRVHG2OAqTmPVRW1n1bOKYhkvPzDkXEg",
-    authDomain: "bartlet-s-bucket-list.firebaseapp.com",
-    databaseURL: "https://bartlet-s-bucket-list.firebaseio.com",
-    projectId: "bartlet-s-bucket-list",
-    storageBucket: "bartlet-s-bucket-list.appspot.com",
-    messagingSenderId: "129357236055"
+
+states = ["AK", "AL", "AR", "AS", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VI", "VT", "WA", "WI", "WV", "WY"]
+for (var i = 0; i < states.length; i++) {
+    var statebutton = $("<a>")
+    console.log(statebutton);
+    statebutton.class("dropdown-item");
+    statebutton.text(states[i]);
+    //statebutton.attr("href", "#");
+    $("#statelist").append(statebutton);
 };
-firebase.initializeApp(config);
-$(document).ready(() => {
-    // Sign-in event
-    $('#sign-in').on('click', event => {
-        event.preventDefault();
-        var email = $('#email').val();
-        var password = $('#password').val();
-        var auth = firebase.auth();
-        var promise = auth.signInWithEmailAndPassword(email, password);
-        promise.catch(event => console.log(event.message));
-    })
 
-    // Sign-up event
-    $('#sign-up').on('click', event => {
-        event.preventDefault();
-        var email = $('#email').val();
-        var password = $('#password').val();
-        var auth = firebase.auth();
-        var promise = auth.createUserWithEmailAndPassword(email, password);
-        promise.catch(event => console.log(event.message));
-    })
 
-    // Sign-out event
-    // $('#sign-out').on('click', event => {
-    //     firebase.auth().signOut();
-    // })
-
-    // Realtime auth listener
-    firebase.auth().onAuthStateChanged(firebaseUser => {
-        if (firebaseUser) {
-            console.log(firebaseUser + 'logged in');
-            // $('#sign-out').removeClass('hide');
-        } else {
-            console.log('not logged in')
-            // $('#sign-out').addClass('hide');
-        }
-    })
-
-})
-var parkcode = "acad"//$(this).val();
-var queryURL = "https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=GlsypCqWXX4ZbgvdJZXJULl2rnm4b18QkUM9oakw";
+var selectstate = "VA" //$(this).val();
+var queryURL = "https://developer.nps.gov/api/v1/parks?stateCode=" + selectstate + "&api_key=GlsypCqWXX4ZbgvdJZXJULl2rnm4b18QkUM9oakw";
 
 $.ajax({
     url: queryURL,
     method: "GET"
 })
 
-.then(function(response) {
-    console.log(data.parkname);
-    var park = $("<div>");
-    var parkname = data.fullname;
-    var city = data.addresses.city;
-    var state = data.address.statecode;
-    $("#parkinformation").append(parkname +"<br>"+ city + ", " + state)
+    .then(function (response) {
+        console.log(response);
+        for (var i = 0; i < response.data.length; i++) {
+            var parkname = $("<p>");
+            parkname.text(response.data[i].fullName);
+            parkname.attr("id", response.data[i].parkCode);
+            $("#topcontent").append(parkname);
+        }
+    });
+var parkcode = "asis" //parkname id
+var queryURL = "https://developer.nps.gov/api/v1/parks?parkCode=" + parkcode + "&api_key=GlsypCqWXX4ZbgvdJZXJULl2rnm4b18QkUM9oakw";
 
-})
-   
+//$("#").on("click", function (event) {})
+
 $.ajax({
     url: queryURL,
-    menthod: "GET"
+    method: "GET"
 })
 
-.then(function(videoresponse){
-    var video = $("<iframe>");
-    var videourl = data.embed_url;
-    var vidieoid =data.vieo_id;
-    vidieoid.attr({
-        src: "videourl",
-        width: "200px",
-        height: "100px",
+    .then(function (response) {
+        console.log(response);
+        var parkinfo = $("<p>");
+        parkinfo.text(response.data["0"].fullName + "p" +
+            response.data["0"].description + "p" +
+            response.data["0"].url + "p" +
+            response.data["0"].weatherInfo);
+        $("#parkinfo").append(parkinfo);
+
     });
-})
+
+
+
