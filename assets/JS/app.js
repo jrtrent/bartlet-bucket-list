2681b3232066b1b28b394ac4bbb788e4882297f3
Martha Ayalew <Marti@Marthas-MacBook-Pro.local>
2018-03-31 13:28:51 -0400

    added CSS

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,14 +1,20 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyAWRVHG2OAqTmPVRW1n1bOKYhkvPzDkXEg",
-    authDomain: "bartlet-s-bucket-list.firebaseapp.com",
-    databaseURL: "https://bartlet-s-bucket-list.firebaseio.com",
-    projectId: "bartlet-s-bucket-list",
-    storageBucket: "bartlet-s-bucket-list.appspot.com",
-    messagingSenderId: "129357236055"
-};
-firebase.initializeApp(config);
+
 $(document).ready(() => {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAWRVHG2OAqTmPVRW1n1bOKYhkvPzDkXEg",
+        authDomain: "bartlet-s-bucket-list.firebaseapp.com",
+        databaseURL: "https://bartlet-s-bucket-list.firebaseio.com",
+        projectId: "bartlet-s-bucket-list",
+        storageBucket: "bartlet-s-bucket-list.appspot.com",
+        messagingSenderId: "129357236055"
+    };
+
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+    var userID = "";
+
     // Sign-in event
     $('#sign-in').on('click', event => {
         event.preventDefault();
@@ -19,26 +25,36 @@ $(document).ready(() => {
         promise.catch(event => console.log(event.message));
     })
 
-    // Sign-up event
-    $('#sign-up').on('click', event => {
-        event.preventDefault();
-        var email = $('#email').val();
-        var password = $('#password').val();
-        var auth = firebase.auth();
-        var promise = auth.createUserWithEmailAndPassword(email, password);
-        promise.catch(event => console.log(event.message));
-    })
+    states = ["AK", "AL", "AR", "AS", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VI", "VT", "WA", "WI", "WV", "WY"]
+    for (var i = 0; i < states.length; i++) {
+        var statebutton = $("<a>")
+        console.log(statebutton);
+        statebutton.class("dropdown-item");
+        statebutton.text(states[i]);
+        //statebutton.attr("href", "#");
+        $("#statelist").append(statebutton);
+    };
 
-    // Sign-out event
-    // $('#sign-out').on('click', event => {
-    //     firebase.auth().signOut();
-    // })
+
+
+   
 
     // Realtime auth listener
     firebase.auth().onAuthStateChanged(firebaseUser => {
+        console.log(firebaseUser);
         if (firebaseUser) {
             console.log(firebaseUser + 'logged in');
+            console.log('UID', firebaseUser.uid);
+            UserID = firebaseUser.uid;
             // $('#sign-out').removeClass('hide');
+            $("#bucket-list").empty();
+            if (!database.ref()){
+                database.ref('users/' + userID).set({
+                    username: name,
+                    email: email
+                    //some more user data
+                });
+            }
         } else {
             console.log('not logged in')
             // $('#sign-out').addClass('hide');
@@ -46,36 +62,49 @@ $(document).ready(() => {
     })
 
 })
-var parkcode = "acad"//$(this).val();
-var queryURL = "https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=GlsypCqWXX4ZbgvdJZXJULl2rnm4b18QkUM9oakw";
+
+// Populate the sidebar with bucket list
+function WriteSidebar(bucketListDB, UID) {
+
+}
+
+var selectstate = "VA" //$(this).val();
+var queryURL = "https://developer.nps.gov/api/v1/parks?stateCode=" + selectstate + "&api_key=GlsypCqWXX4ZbgvdJZXJULl2rnm4b18QkUM9oakw";
 
 $.ajax({
     url: queryURL,
     method: "GET"
 })
 
-.then(function(response) {
-    console.log(data.parkname);
-    var park = $("<div>");
-    var parkname = data.fullname;
-    var city = data.addresses.city;
-    var state = data.address.statecode;
-    $("#parkinformation").append(parkname +"<br>"+ city + ", " + state)
+    .then(function (response) {
+        console.log(response);
+        for (var i = 0; i < response.data.length; i++) {
+            var parkname = $("<p>");
+            parkname.text(response.data[i].fullName);
+            parkname.attr("id", response.data[i].parkCode);
+            $("#topcontent").append(parkname);
+        }
+    });
+var parkcode = "asis" //parkname id
+var queryURL = "https://developer.nps.gov/api/v1/parks?parkCode=" + parkcode + "&api_key=GlsypCqWXX4ZbgvdJZXJULl2rnm4b18QkUM9oakw";
 
-})
-   
+//$("#").on("click", function (event) {})
+
 $.ajax({
     url: queryURL,
-    menthod: "GET"
+    method: "GET"
 })
 
-.then(function(videoresponse){
-    var video = $("<iframe>");
-    var videourl = data.embed_url;
-    var vidieoid =data.vieo_id;
-    vidieoid.attr({
-        src: "videourl",
-        width: "200px",
-        height: "100px",
+    .then(function (response) {
+        console.log(response);
+        var parkinfo = $("<p>");
+        parkinfo.text(response.data["0"].fullName + "p" +
+            response.data["0"].description + "p" +
+            response.data["0"].url + "p" +
+            response.data["0"].weatherInfo);
+        $("#parkinfo").append(parkinfo);
+
     });
-})
+
+
+
